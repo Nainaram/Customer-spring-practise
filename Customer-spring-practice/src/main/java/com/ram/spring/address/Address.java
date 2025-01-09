1ab01d5e87318171566e66babce8b3666f2dc529
NainaRam <92089901+Nainaram@users.noreply.github.com>
2025-01-09 19:26:16 +0530

    constructor injection

--- Customer-spring-practice/src/main/java/com/ram/spring/address/Address.java
+++ Customer-spring-practice/src/main/java/com/ram/spring/address/Address.java
@@ -4,20 +4,27 @@ public class Address {
 
 	private String city;
 	private String state;
+	
+
+	public Address(String city, String state) {
+		super();
+		this.city = city;
+		this.state = state;
+	}
 
 	public String getCity() {
 		return city;
 	}
 
-	public void setCity(String city) {
-		this.city = city;
-	}
+//	public void setCity(String city) {
+//		this.city = city;
+//	}
 
 	public String getState() {
 		return state;
 	}
 
-	public void setState(String state) {
-		this.state = state;
-	}
+//	public void setState(String state) {
+//		this.state = state;
+//	}
 }
--- Customer-spring-practice/src/main/java/com/ram/spring/main/Main.java
+++ Customer-spring-practice/src/main/java/com/ram/spring/main/Main.java
@@ -17,6 +17,6 @@ public class Main {
 
 		gc.defCustomer();
 		pc.defCustomer();
-
+		cpx.close();
 	}
 }
